f4a42cf32f1d7cabee65b73078b2b1db1bb7a6ba
caohang <1023819715@qq.com>
2019-05-31 09:54:59 +0800

    消息组件列表

--- src/views/control/control.js
+++ src/views/control/control.js
@@ -1,13 +1,18 @@
-import {AccountManage} from './account'
-import React from 'react'
-import {Divider} from 'antd'
-import {InfoCard} from './infoCard'
-export function Control(){
-    return (
-        <>
-        <AccountManage/>
-        <Divider/>
-        <InfoCard/>
-        </>
-    )
+import { AccountManage } from "./account";
+import React from "react";
+import { Divider } from "antd";
+import { InfoCard } from "./infoCard";
+export function Control() {
+  const cardList = [
+    { title: "项目通知", number: 4 },
+    { title: "官方通知", number: 3 },
+    { title: "人员通知", number: 4 }
+  ];
+  return (
+    <>
+      <AccountManage />
+      <Divider />
+      <InfoCard cardList={cardList}/>
+    </>
+  );
 }
--- src/views/control/infoCard.js
+++ src/views/control/infoCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {Card,Menu} from 'antd'
-export function InfoCard(){
+import {Card} from 'antd'
+function CardRow(props){
+    return (
+        <div className='message-row'>
+            <span>{props.title}</span>
+            <span>{props.number}</span>
+        </div>
+    )
+}
+export function InfoCard(props){
+    const list=props.cardList.map((value,index)=>{
+        return <CardRow {...value} key={index}></CardRow>
+    })
     return (
         <>
-            <Card title='未读消息' style={{width:300}}>
-                <div>项目通知</div>
-                <div>官方通知</div>
-                <div>人员通知</div>
+            <Card title='未读消息' style={{width:300}} className='info-card'>
+                {list}
             </Card>
         </>
     )
