f7679ddc96ca1e38d3eead43cc380d1a6345428b
caohang <1023819715@qq.com>
2019-06-05 09:32:58 +0800

    api研发管理

--- src/views/api/manage.js
+++ src/views/api/manage.js
@@ -1,6 +1,29 @@
 import React from 'react'
+import {Table,Button} from 'antd'
+const {Column} =Table
 export function Manage(){
+    const dataSource=[{
+        name:'测试',
+        version:'1.0',
+        type:'web',
+        lastModified:'2019-06-04 13:00',
+        key:1
+    }]
     return (
-        <>manage</>
+        <>
+            <Table dataSource={dataSource}>
+                <Column title='名称' dataIndex='name' key='name'></Column>
+                <Column title='版本号' dataIndex='version' key='version'></Column>
+                <Column title='类型' dataIndex='type' key='type'></Column>
+                <Column title='最后更新时间' dataIndex='lastModified' key='lastModified'></Column>
+                <Column title='操作' render={()=>{
+                    return <>
+                        <Button type='link'>编辑</Button>
+                        <Button type='link'>移动</Button>
+                        <Button type='link'>删除</Button>
+                    </>
+                }} key='action'></Column>
+            </Table>
+        </>
     )
 }
--- src/views/api/topAction.js
+++ src/views/api/topAction.js
@@ -2,36 +2,35 @@ import React from "react";
 import { Button, Menu, Dropdown, Icon, Input, Select } from "antd";
 const menu = (
   <Menu>
-    <Menu.Item key="1">1st item</Menu.Item>
-    <Menu.Item key="2">2nd item</Menu.Item>
-    <Menu.Item key="3">3rd item</Menu.Item>
+    <Menu.Item key="1">新建项目</Menu.Item>
+    <Menu.Item key="2">新建项目组</Menu.Item>
   </Menu>
 );
 const Group = Input.Group;
-const Search=Input.Search
-const {Option}=Select
+const Search = Input.Search;
+const { Option } = Select;
 export function TopAction() {
   return (
     <>
       <p>API研发管理</p>
-      <div className='api-top'>
-        <Dropdown overlay={menu}>
-          <Button type="primary" icon="plus">
-            新建<Icon type="down" />
+      <div className="api-top">
+        <div className="left-top">
+          <Dropdown overlay={menu}>
+            <Button type="primary" icon="plus">
+              新建
+              <Icon type="down" />
+            </Button>
+          </Dropdown>
+          <Button type="link" icon="appstore">
+            批量操作
           </Button>
-        </Dropdown>
-        <Button type="link" icon="appstore">
-          批量操作
-        </Button>
+        </div>
         <Group compact>
           <Select defaultValue="all">
             <Option value="all">全部</Option>
             <Option value="API">API</Option>
           </Select>
-          <Search
-          placeholder='搜索'
-          style={{width:200}}
-          />
+          <Search placeholder="搜索" style={{ width: 200 }} />
         </Group>
       </div>
     </>
