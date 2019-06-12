0057a1adbfa1009baf326b87545b627a6c356286
caohang <1023819715@qq.com>
2019-06-12 09:09:26 +0800

    格式化

--- src/views/control/account.js
+++ src/views/control/account.js
@@ -1,19 +1,22 @@
-import {Button,Icon,Card} from 'antd'
-import React,{useState,useContext} from 'react'
-import {AccountModal} from './accountModal'
-import './control.scss'
-import {UserCtx} from '../../App'
-export function AccountManage(){
-    const [modalVisible,setModalVisible]=useState(false)
-    const {userInfo}=useContext(UserCtx)
-    return (
-        <>
-        <Card className='accountManage' >
-            <Icon type='user'/>
-            <span style={{margin:'0 24px'}}>{userInfo.name}</span>
-            <Button onClick={()=>setModalVisible(true)}>账户管理</Button>
-        </Card>
-        <AccountModal visible={modalVisible} hide={()=>setModalVisible(false)}/>
-        </>
-    )
+import { Button, Icon, Card } from "antd";
+import React, { useState, useContext } from "react";
+import { AccountModal } from "./accountModal";
+import "./control.scss";
+import { UserCtx } from "../../App";
+export function AccountManage() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const { userInfo } = useContext(UserCtx);
+  return (
+    <>
+      <Card className="accountManage">
+        <Icon type="user" />
+        <span style={{ margin: "0 24px" }}>{userInfo.name}</span>
+        <Button onClick={() => setModalVisible(true)}>账户管理</Button>
+      </Card>
+      <AccountModal
+        visible={modalVisible}
+        hide={() => setModalVisible(false)}
+      />
+    </>
+  );
 }
