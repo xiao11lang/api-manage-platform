22a7b9ce2d6c901e7d61568e57af05ba140cd559
caohang <1023819715@qq.com>
2019-08-26 17:20:12 +0800

    通用返回组件

---
+++ src/components/generalBack.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Icon, Button } from 'antd'
+export default function GeneralBack(props) {
+  return (
+    <div className="general-top-save">
+      <div onClick={props.hide}>
+        <Icon type="rollback" className="right-10" />
+        <span>返回API列表</span>
+      </div>
+      {props.simple ? null : (
+        <Button type="primary" className="left-10" onClick={props.save}>
+          保存
+        </Button>
+      )}
+    </div>
+  )
+}
